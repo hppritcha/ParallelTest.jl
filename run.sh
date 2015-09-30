9b63b1d1748845e16ef4440c3973b2d7d7c88bd9
Ian Czekala <iancze@gmail.com>
2015-09-30 14:33:01 -0400

    spacing wrong

--- run.sh
+++ run.sh
@@ -33,4 +33,4 @@ echo "Worker CPUs are $CPULIST"
 
 #Constrain the brain process to start on CPUMASTER, then add the workers in using AffinityManager.
 # taskset -c $CPUMASTER ./add_workers_manager.jl $CPULIST
-./add_workers_manager.jl $CPULIST
+./add_workers_manager.jl --cpus $CPULIST
