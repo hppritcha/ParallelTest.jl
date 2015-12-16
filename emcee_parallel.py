672202dc0f2602ac187927cea9e41aff3cc41015
Ian Czekala <iancze@gmail.com>
2015-12-16 17:20:04 -0500

    hopefully printing info now.

--- emcee_parallel.py
+++ emcee_parallel.py
@@ -4,12 +4,19 @@
 
 import numpy as np
 import emcee
+import subprocess
 
 def lnprob(p, ivar):
 
     #Add a significant computational component that may require launching command line scripts.
     for i in range(2000):
         a = i*10
+
+    p1 = subprocess.Popen(["ps", "-eo", "pid,psr,pcpu,cmd"], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["grep", "julia"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close()
+    output = p2.communicate()[0]
+    print(output)
 
     return -0.5 * np.sum(ivar * p ** 2)
 
